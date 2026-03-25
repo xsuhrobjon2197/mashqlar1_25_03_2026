aa9b5d8efda7aa17b50a5575f6271b52c406db79
xsuxrobjon2197 <xsuhrobjon2197@gmail.com>
2026-03-25 16:15:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,126 @@
+1-m
+a = 10
+res = ["musbat"]
+if a > 0:
+    print("musbat")
+else:
+    print("manfiy")
+
+holat = lambda son:
+res = holat(10)
+
+2-m
+son = int(input("Son kiriting: "))
+
+natija = "juft" if son % 2 == 0 else "toq"
+print(natija)
+
+#3-m
+son = int(input("son kirit: "))
+
+natija = "katta" if son > 100 else "kichik"
+print(natija)
+
+#4-m
+son = int(input("son kirit: "))
+
+natija = "kichik" if son < 0 else "katta"
+print(natija)
+
+#5-m
+son = int(input("son kirit: "))
+
+result = "katta" if son > 10 else "kichik"
+print(result)
+
+#6-m
+son = int(input("son kirit: "))
+
+result = "teng" if son == 0 else "teng emas"
+print(result)
+
+#7-m
+son = int(input("son kirit: "))
+
+result =  "katta" if son > 100 else "Kichik"
+print(result)
+
+#8-m
+import math
+
+funk = lambda  son: math.fabs(son) if son < 0 else  son
+print(funk)
+
+#9-m
+a = int(input("Son kiriting: "))
+b = int(input("Son kiriting: "))
+
+dfyu = "teng" if a ==  b   else "teng emas"
+print(dfyu)
+
+#10-m
+a = int(input("son kirit: "))
+
+argen = "karrali" if a % 5 == 0 else "karrasiz"
+print(argen)
+
+#11-m
+a = int(input("Son kirit: "))
+b = int(input("Son kirit: "))
+c = int(input("Son kirit: "))
+
+cse = "Katta" if a > 0 else "kichik"
+print(cse)
+
+#12-m
+a = int(input("Son kirit: "))
+
+x = "adult" if a > 18 else "minor"
+print(x)
+
+#13
+a = int(input("Son kirit: "))
+
+x = "positeve" if a > 0 else "negative"
+print(x)
+
+#14-m
+a = int(input("Son kirit: "))
+
+x = "yessssss" if a % 2 == 0 else "no"
+print(x)
+
+#15-m
+a = int(input("Son kirit: "))
+
+x = "yaqin" if a < 10 else "uzoq"
+print(x)
+
+#16-m
+a = int(input("Son kirit: "))
+
+x = "javob" if a * 2 else a % 2 == 0
+print(x)
+
+#17-m
+a = int(input("Son kirit: "))
+
+x = "oshirish" if a * 2 else a % 1== 0
+print(x)
+
+#18-m
+a = int(input("Son kirit: "))
+b = int(input("Son kirit: "))
+
+x = "katta" if a * b < 100 else "kichik"
+
+#19-m
+a = int(input("Son kirit: "))
+
+x = "yesss" if 1 < a > 10 else "no"
+print(x)
+
+#20-m
+a = int(input("Son kirit: "))
+
+x = "bo'lish" if a % 3 % 5 == 0 else "bo'linmaydi"
